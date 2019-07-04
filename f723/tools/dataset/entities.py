ca92a49e00d690f3e9de538b5fa58325c1e1df62
Manakov Alexander <kokokostation@gmail.com>
2019-07-04 13:55:31 +0300

    fix

--- f723/tools/dataset/entities.py
+++ f723/tools/dataset/entities.py
@@ -12,14 +12,26 @@ BasePair = namedtuple('BasePair', 'nt_left nt_right type')
 BasePairType = namedtuple('BasePairType', 'saenger lw dssr in_ss')
 
 
-def make_base_pair(nt_left, nt_right, bp_type=None):
+def normalize_pair(nt_left, nt_right):
     if nt_left.chain_id == nt_right.chain_id and nt_left.index > nt_right.index:
         warning_message = 'For base pair ({}, {}): nt_left.index > nt_right.index'.format(nt_left, nt_right)
         logger.warning(warning_message)
 
         nt_left, nt_right = nt_right, nt_left
 
+    return nt_left, nt_right
+
+
+def make_base_pair(nt_left, nt_right, bp_type):
+    nt_left, nt_right = normalize_pair(nt_left, nt_right)
+
     return BasePair(nt_left=nt_left, nt_right=nt_right, type=bp_type)
+
+
+def make_pair(nt_left, nt_right):
+    nt_left, nt_right = normalize_pair(nt_left, nt_right)
+
+    return Pair(nt_left=nt_left, nt_right=nt_right)
 
 
 NucleotideFeatures = namedtuple('NucleotideFeatures', 'secondary_structure base fragment_length fragment_index')
